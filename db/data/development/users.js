2243e45209bc580eea41bd48fa38a7ae064d3ef3
“Tayyab <tayyabhaseeb5@gmail.com>
2025-07-04 16:15:12 +0100

    adds users

---
+++ db/data/development/users.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    username: "tayyab_haseeb",
+    name: "Tayyab Haseeb",
+    avatar_url: "https://i.pravatar.cc/150?img=11",
+    email: "tayyab@example.com",
+  },
+  {
+    username: "jane_doe",
+    name: "Jane Doe",
+    avatar_url: "https://i.pravatar.cc/150?img=12",
+    email: "jane@example.com",
+  },
+  {
+    username: "omar_farooq",
+    name: "Omar Farooq",
+    avatar_url: "https://i.pravatar.cc/150?img=13",
+    email: "omar@example.com",
+  },
+  {
+    username: "sofia_ali",
+    name: "Sofia Ali",
+    avatar_url: "https://i.pravatar.cc/150?img=14",
+    email: "sofia@example.com",
+  },
+  {
+    username: "liam_nguyen",
+    name: "Liam Nguyen",
+    avatar_url: "https://i.pravatar.cc/150?img=15",
+    email: "liam@example.com",
+  },
+];
