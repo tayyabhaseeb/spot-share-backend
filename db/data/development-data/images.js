2129f2fdb2ec44093c5103a7a727084af8b00b21
“Tayyab <tayyabhaseeb5@gmail.com>
2025-07-04 18:34:47 +0100

    sets the data

---
+++ db/data/development-data/images.js
@@ -0,0 +1,52 @@
+const images = [
+  {
+    url: "https://images.unsplash.com/photo-1561489429-3b4d5162b20e",
+    location: "Heaton Park",
+    place_id: 1,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1508672019048-805c876b67e2",
+    location: "Heaton Park",
+    place_id: 1,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be",
+    location: "Manchester Central Library",
+    place_id: 2,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d",
+    location: "Manchester Central Library",
+    place_id: 2,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1603038224125-074cd8913fd3",
+    location: "Salford Quays",
+    place_id: 3,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1542223616-71f1a6b4b21a",
+    location: "Salford Quays",
+    place_id: 3,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1593697820903-d39ed96f4c49",
+    location: "Whitworth Art Gallery",
+    place_id: 4,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1551183053-bf91a1d81141",
+    location: "John Rylands Library",
+    place_id: 5,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1586085791362-b6fa6c2f1ab8",
+    location: "Platt Fields Park",
+    place_id: 6,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1605296867304-46d5465a13f1",
+    location: "Peel Park",
+    place_id: 7,
+  },
+];
---
+++ db/data/test-data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    username: "tayyab_haseeb",
+    name: "Tayyab Haseeb",
+    avatar_url: "https://i.pravatar.cc/150?img=11",
+    email: "tayyab@example.com",
+  },
+  {
+    username: "jane_doe",
+    name: "Jane Doe",
+    avatar_url: "https://i.pravatar.cc/150?img=12",
+    email: "jane@example.com",
+  },
+  {
+    username: "omar_farooq",
+    name: "Omar Farooq",
+    avatar_url: "https://i.pravatar.cc/150?img=13",
+    email: "omar@example.com",
+  },
+];
